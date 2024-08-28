4204639dc8d41136f79100ff7d66dfde70491a39
Guilherme Cugler <guilhermecugler@gmail.com>
2024-08-28 15:28:49 -0300

    upt

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import os
 import requests
 import pandas as pd
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from dotenv import load_dotenv
 import json
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from dotenv import load_dotenv
 
-# Carrega as variáveis de ambiente (apenas para testes locais)
+# Carrega as variáveis de ambiente
 load_dotenv()
 
 # Função para baixar o arquivo XLSX
@@ -20,20 +18,15 @@ def baixar_arquivo_xlsx(url, nome_arquivo):
 
 # Função para atualizar Google Sheets
 def atualizar_google_sheet(nome_arquivo, sheet_name, sheet_id, credenciais_json):
-    # Cria o fluxo OAuth 2.0
-    flow = InstalledAppFlow.from_client_config(
-        json.loads(credenciais_json),
-        scopes=['https://www.googleapis.com/auth/spreadsheets']
-    )
-
-    # Realiza a autenticação e obtém as credenciais
-    creds = flow.run_local_server(port=0)
-
-    # Construa o serviço
-    service = build('sheets', 'v4', credentials=creds)
+    # Carrega as credenciais do JSON diretamente da string da variável de ambiente
+    creds_dict = json.loads(credenciais_json)
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    client = gspread.authorize(creds)
 
     # Abre a planilha pelo ID
-    sheet = service.spreadsheets()
+    sheet = client.open_by_key(sheet_id)
+    worksheet = sheet.worksheet(sheet_name)
 
     # Carrega os dados do XLSX usando Pandas
     df = pd.read_excel(nome_arquivo)
@@ -42,15 +35,8 @@ def atualizar_google_sheet(nome_arquivo, sheet_name, sheet_id, credenciais_json)
     dados = [df.columns.values.tolist()] + df.values.tolist()
 
     # Atualiza a Google Sheet com os dados
-    body = {
-        'values': dados
-    }
-    sheet.values().update(
-        spreadsheetId=sheet_id,
-        range=sheet_name,
-        valueInputOption='RAW',
-        body=body
-    ).execute()
+    worksheet.clear()
+    worksheet.update(dados)
     print("Google Sheet atualizada com sucesso.")
 
 # Função principal que combina o download e atualização
@@ -61,6 +47,8 @@ def executar_tarefa():
     sheet_name = os.getenv("SHEET_NAME")
     sheet_id = os.getenv("SHEET_ID")
     credenciais_json = os.getenv("GOOGLE_CREDENTIALS_JSON")  # Carrega o JSON das credenciais como string
+    teste = json.loads(credenciais_json)
+    print(teste)
 
     # Executa o download e atualização da planilha
     baixar_arquivo_xlsx(url_arquivo, nome_arquivo)
